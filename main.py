1437316fdbbc79c516da98a1c79cd99948b48910
joeugimenez <jemergimenez@gmail.com>
2025-05-12 20:52:00 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Jornada de Dados")
+
+#variaveis
+idade = 5
+nome = "Eugenio"
+
+#print
+print(idade)
+
+#soma
+idadenova = idade + 1
+print(idadenova)
+
+#listas
+lista=[1,2,3,4,5]
+
+#metodos
+lista_invertida = lista.reverse()
+
